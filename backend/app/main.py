c3210aeaf34e47c3feef4f93ef710bd7a73f6102
Natalia <67316260+npolyakova@users.noreply.github.com>
2024-12-17 23:35:54 +0300

    Add routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Request, Security, Depends, HTTPException, Body, Query
+from fastapi import FastAPI, Request, Security, Depends, HTTPException, Query
 from fastapi.security import APIKeyHeader
+import psycopg2
 import jwt
 from jwt import InvalidTokenError
-
-from app.config import JWT_SECRET
-from app import queries
-
+from config import DATABASE_CONFIG
+from config import JWT_SECRET
+import queries
 
 # Middleware/зависимость для проверки токена
 async def check_access_token(
@@ -55,10 +55,8 @@ def read_root():
 
 #Авторизация с генерацией токена и его возвратом
 @app.post("/auth")
-async def get_user_token(data = Body()):
+async def get_user_token(login: str, password: str):
     # Используем вынесенную функцию для выполнения SQL-запроса
-    login = data["login"]
-    password = data["password"]
     user = queries.auth_sql_query(login)
 
     if not user:
@@ -89,16 +87,11 @@ async def get_rooms_list(request: Request, token: str = Depends(check_access_tok
         rooms_list = []
         for room in rooms:
             bounds = room[3]
-
-            bounds_items = []
-            for item in bounds:
-                bounds_items = list(map(int, item.split()))
-
-            if bounds_items and len(bounds_items) % 2 == 0:
-                bounds_pairs = [{"x": int(bounds_items[i]), "y": int(bounds_items[i + 1])} for i in
-                                range(0, len(bounds_items), 2)]
+            
+            if bounds and len(bounds) % 2 == 0:
+                bounds_pairs = [{"x": int(bounds[i]), "y": int(bounds[i + 1])} for i in range(0, len(bounds), 2)]
             else:
-                bounds_pairs = []
+                bounds_pairs = [] 
             
             rooms_list.append({
                 "name": room[0],
@@ -113,3 +106,9 @@ async def get_rooms_list(request: Request, token: str = Depends(check_access_tok
             "user_id": request.state.user,
             "query": query,
         }
+
+#Строим маршрут
+@app.get("/route")
+def get_route(roomstart:int, roomend:int):
+	route = queries.get_rooms_point(roomstart,roomend)
+	return {"route": route}
--- backend/app/queries.py
+++ backend/app/queries.py
@@ -1,10 +1,15 @@
+from config import DATABASE_CONFIG
+from collections import defaultdict
+from collections import deque
 import psycopg2
-
-from app.config import DATABASE_CONFIG
-
 # Подключение к базе данных
 try:
-    conn = psycopg2.connect(f'postgresql://{DATABASE_CONFIG["user"]}:{DATABASE_CONFIG["password"]}@postgres:5432/{DATABASE_CONFIG["dbname"]}')
+    conn = psycopg2.connect(
+        dbname=DATABASE_CONFIG["dbname"],
+        user=DATABASE_CONFIG["user"],
+        password=DATABASE_CONFIG["password"],
+        host=DATABASE_CONFIG["host"],
+    )
 except Exception as e:
     print(f"Failed to connect to the database: {e}")
     conn = None
@@ -36,3 +41,62 @@ def get_rooms_sql_query(query: str):
             curs.execute("SELECT name, floor, id, map_points FROM rooms")
         rooms = curs.fetchall()
     return rooms
+
+def get_rooms_point(start: int, end: int):
+    with conn.cursor() as curs:
+        curs.execute("SELECT id_route_point FROM rooms WHERE id IN (%s, %s)",(start, end))
+        rooms_point_id = curs.fetchall()
+    (point_id_r1, point_id_r2) = rooms_point_id[0][0], rooms_point_id[1][0]
+
+    #with conn.cursor() as curs:
+    #    curs.execute("SELECT points FROM route_points WHERE id IN (%s, %s)",(point_id_r1, point_id_r2))
+    #    rooms_point = curs.fetchall()
+    #(start_point, end_point) = rooms_point[0][0], rooms_point[1][0]
+    #routes_list = []
+    #routes_list.append({
+    #    "start": start_point,
+    #    "end": end_point
+    #})
+    graph_data = get_graph_data()
+    routes_list_id =  bfs(graph_data, point_id_r1, point_id_r2)
+    with conn.cursor() as curs:
+        # Создаём плейсхолдеры для каждого ID
+        placeholders = ', '.join(['%s'] * len(routes_list_id))
+        query = f"SELECT points FROM route_points WHERE id IN ({placeholders})"
+    
+        # Выполняем запрос с распакованным списком
+        curs.execute(query, tuple(routes_list_id))
+        routes_list = curs.fetchall()
+    return routes_list
+
+# BFS для поиска кратчайшего пути
+def bfs(graph, start, end):
+    queue = deque([[start]])
+    visited = set()
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+        if node == end:
+            return path
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    return None
+
+def get_graph_data():
+    graph = defaultdict(list)
+    with conn.cursor() as curs:
+        curs.execute("SELECT id, connected_points FROM route_points WHERE connected_points IS NOT NULL")
+        for row in curs.fetchall():
+            node_id = int(row[0])
+            connections = row[1]
+            if isinstance(connections, list):
+                graph[node_id] = [int(conn) for conn in connections]
+            else:
+                connections_list = [int(float(x)) for x in connections.strip('{}').split(',')]
+                graph[node_id] = connections_list
+    return graph
