ce2682e7a71f5693ffdef203342f040dd81ff93f
EryginYaroslav <kodneim47@gmail.com>
2024-12-17 23:08:13 +0300

    apiRoutes

--- backend/queries.py
+++ backend/queries.py
@@ -1,4 +1,6 @@
 from config import DATABASE_CONFIG
+from collections import defaultdict
+from collections import deque
 import psycopg2
 # Подключение к базе данных
 try:
@@ -39,3 +41,62 @@ def get_rooms_sql_query(query: str):
             curs.execute("SELECT name, floor, id, map_points FROM rooms")
         rooms = curs.fetchall()
     return rooms
+
+def get_rooms_point(start: int, end: int):
+    with conn.cursor() as curs:
+        curs.execute("SELECT id_route_point FROM rooms WHERE id IN (%s, %s)",(start, end))
+        rooms_point_id = curs.fetchall()
+    (point_id_r1, point_id_r2) = rooms_point_id[0][0], rooms_point_id[1][0]
+
+    #with conn.cursor() as curs:
+    #    curs.execute("SELECT points FROM route_points WHERE id IN (%s, %s)",(point_id_r1, point_id_r2))
+    #    rooms_point = curs.fetchall()
+    #(start_point, end_point) = rooms_point[0][0], rooms_point[1][0]
+    #routes_list = []
+    #routes_list.append({
+    #    "start": start_point,
+    #    "end": end_point
+    #})
+    graph_data = get_graph_data()
+    routes_list_id =  bfs(graph_data, point_id_r1, point_id_r2)
+    with conn.cursor() as curs:
+        # Создаём плейсхолдеры для каждого ID
+        placeholders = ', '.join(['%s'] * len(routes_list_id))
+        query = f"SELECT points FROM route_points WHERE id IN ({placeholders})"
+    
+        # Выполняем запрос с распакованным списком
+        curs.execute(query, tuple(routes_list_id))
+        routes_list = curs.fetchall()
+    return routes_list
+
+# BFS для поиска кратчайшего пути
+def bfs(graph, start, end):
+    queue = deque([[start]])
+    visited = set()
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+        if node == end:
+            return path
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    return None
+
+def get_graph_data():
+    graph = defaultdict(list)
+    with conn.cursor() as curs:
+        curs.execute("SELECT id, connected_points FROM route_points WHERE connected_points IS NOT NULL")
+        for row in curs.fetchall():
+            node_id = int(row[0])
+            connections = row[1]
+            if isinstance(connections, list):
+                graph[node_id] = [int(conn) for conn in connections]
+            else:
+                connections_list = [int(float(x)) for x in connections.strip('{}').split(',')]
+                graph[node_id] = connections_list
+    return graph
